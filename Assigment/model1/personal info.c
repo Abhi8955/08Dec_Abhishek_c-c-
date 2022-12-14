630ab79d41bd92951a1c25355b58cd23d8a3b047
Abhi8955 <120082500+Abhi8955@users.noreply.github.com>
2022-12-14 14:42:09 +0530

    Add files via upload

---
+++ Assigment/model1/personal info.c
@@ -0,0 +1,10 @@
+//wap personal information
+#include<stdio.h>
+void main()
+{
+	printf("Name:- trivedi abhishek\n");
+	printf("Birthdate:- 01/10/2001\n");
+	printf("Age:- 21\n");
+	printf("Adress:- Shashtrinagar nanamova rajkot 360004\n");	
+}
+
