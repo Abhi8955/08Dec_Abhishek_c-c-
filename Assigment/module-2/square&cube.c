1af65030a1d8f7bc212dd6ebc01a8cff4c89357e
Abhi8955 <120082500+Abhi8955@users.noreply.github.com>
2022-12-23 11:24:11 +0530

    Add files via upload

---
+++ Assigment/module-2/square&cube.c
@@ -0,0 +1,21 @@
+//wap square & cube
+#include<stdio.h>
+int main()
+{
+	int no,s,c;
+	printf("enter the no:");
+	scanf("%d",&no);
+    
+	printf("----------square----------");
+	s=no*no;
+	printf("\n square:%d",s);
+	
+	c=no*no*no;
+	printf("\n----------cube----------");
+    printf("\n cube:%d",c);
+	
+	return 0;
+	
+	
+	
+}
