9a72c3109243bc26883d68a7c32aab527b2676c7
Abhi8955 <120082500+Abhi8955@users.noreply.github.com>
2023-02-02 18:03:56 +0530

    Add files via upload

---
+++ task/matrix addition.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{ 
+    int i,j,n,m; 
+    int a[3][3]={1,2,3,4,5,6,7,8,9},b[3][3]={1,2,3,4,5,6,7,8,9},c[3][3]; 
+    for (i = 0; i < 3; i++) 
+    { 
+        for (j = 0; j < 3; j++) 
+        { 
+            printf("%d ",a[i][j]); 
+        } 
+      printf("\n");
+     } 
+    for (i = 0; i < 3;i++) 
+    { 
+        for (j = 0; j < 3; j++) 
+        { 
+            printf("%d ",b[i][j]); 
+        } 
+      printf("\n");
+     } 
+  for (i = 0; i < 3; i++) 
+    { 
+        for (j = 0; j < 3; j++) 
+        { 
+            c[i][j]=a[i][j]+b[i][j];
+            printf("%d ",c[i][j]); 
+        } 
+      printf("\n");
+     } 
+    return 0;
+}
+
+          
+
+
